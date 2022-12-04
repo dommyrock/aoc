c911364575bd022753e0fc973ff4dd348a508379
Dominik Polzer <dominik.polzer@fivedotagency.onmicrosoft.com>
2022-12-04 16:45:53 +0100

    day4

--- aoc_2022/src/lib.rs
+++ aoc_2022/src/lib.rs
@@ -2,19 +2,66 @@
 cargo test -- --show-output
 cargo test -- --nocapture
 */
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::{
         collections::{HashMap, HashSet},
         fs::File,
-        io::{BufRead, BufReader, Result},
+        io::{BufRead, BufReader, Result, Split},
         ops::RangeInclusive,
     };
     const INPUT_PATH: &str = "D:\\Me\\Git\\aoc\\aoc_2022\\src\\inputs";
 
     #[test]
+    fn task4_1_2() -> Result<()> {
+        let (mut overlap2, mut overlap1) = (0, 0);
+        let range_pairs: Vec<Vec<i32>> = include_str!("./inputs/4.txt")
+            .split("\r\n")
+            .map(|x| x.split(',').collect::<Vec<&str>>())
+            .flat_map(|y| y)
+            .map(|x| x.split('-').collect::<Vec<&str>>())
+            .map(|v| v.into_iter().map(|w| w.parse::<i32>().unwrap()).collect::<Vec<i32>>())
+            .collect();
+
+        for pair in range_pairs.chunks(2) {
+            let (lr_start, lr_end) = (pair[0].get(0).unwrap(), pair[0].get(1).unwrap());
+            let (rr_start, rr_end) = (pair[1].get(0).unwrap(), pair[1].get(1).unwrap());
+
+            if lr_start <= rr_end && lr_end >= rr_start {
+                overlap2 += 1;
+            }
+            if [lr_start, lr_end].iter().all(|x| (rr_start..=rr_end).contains(x))
+            || [rr_start, rr_end].iter().all(|x| (lr_start..=lr_end).contains(x))
+            {
+                overlap1 += 1;
+            }
+        }
+        println!("p1 > {:?}", overlap1);
+        println!("p2 > {:?}", overlap2);
+        Ok(())
+    }
+
+    fn range_inclusive(a: usize, b: usize) -> impl Iterator<Item = usize> {
+        let x: Box<dyn Iterator<Item = usize>>;
+        if b > a {
+            x = Box::new(a..=b)
+        } else {
+            x = Box::new((b..=a).rev())
+        }
+        x
+    }
+
+    #[test]
+    #[ignore]
+    fn testing_ranges_start_gt_end() {
+        for i in range_inclusive(3, 1).zip(range_inclusive(1, 3)) {
+            println!("{:?}", i);
+        }
+    }
+
+    #[test]
+    #[ignore]
     fn task3_2() -> Result<()> {
         let mut sum = 0;
         let (dict_l, dict_u) = get_hashsets([('a'..='z'), ('A'..='Z')]);
@@ -27,10 +74,10 @@ mod tests {
         let groups = rows.chunks(3);
 
         for chunk in groups {
-            let first: HashSet<char> = chunk.first().unwrap().chars().collect();
-            for item in first {
-                if let (Some(first), Some(_)) = (chunk[1].find(item), chunk[2].find(item)) {
-                    let team_mark = chunk[1].chars().nth(first).unwrap();
+            let first_of_group: HashSet<char> = chunk.first().unwrap().chars().collect();
+            for item in first_of_group {
+                if let (Some(second), Some(_third)) = (chunk[1].find(item), chunk[2].find(item)) {
+                    let team_mark = chunk[1].chars().nth(second).unwrap();
                     match team_mark.is_lowercase() {
                         true => sum += *dict_l.get(&team_mark).unwrap(),
                         false => sum += *dict_u.get(&team_mark).unwrap(),
@@ -163,6 +210,21 @@ mod tests {
         println!("Score > {}", score);
         Ok(())
     }
+    #[test]
+    #[ignore]
+    fn task1_2_prime() -> Result<()> {
+        let mut max = include_str!("./inputs/1.txt")
+            .split("\r\n\r\n") //on linux it's \n\n
+            .map(|x| {
+                return x.lines().flat_map(str::parse::<usize>).sum::<usize>();
+            })
+            .collect::<Vec<usize>>();
+
+        max.sort_by(|a, b| b.cmp(a));
+
+        println!("max me daddy {:?}", max.into_iter().take(3).sum::<usize>());
+        return Ok(());
+    }
 
     #[test]
     #[ignore]
@@ -236,6 +298,10 @@ FromStr     https://doc.rust-lang.org/std/str/trait.FromStr.html
 
 //Maping range to Hashmap
     (1..5).map(|i| (i + i, i * i)).collect::<HashMap<_, _>>()
+
+//Chan and Vec<_>.split
+ strmax.map(|x| x.split('-')).chain(other);//chain is for iterators over 2x collections at once
+.split('-'); could be called on Vec to conditionaly exclude items from it
 
 */
 
