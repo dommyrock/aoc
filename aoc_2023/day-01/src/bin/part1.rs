79cb7d611ad9b46e09bd474e9b5d39fab78294f8
dommyrock <dominik.polzer@hotmail.com>
2023-12-01 17:53:14 +0100

    day 1

--- aoc_2023/day-01/src/bin/part1.rs
+++ aoc_2023/day-01/src/bin/part1.rs
@@ -1,10 +1,35 @@
 fn main() {
+    solution();
+}
+fn solution() {
     let input = include_str!("./input1.txt");
-    println!("{input}!");
+    let mut res = Vec::<i32>::new();
+
+    input.lines().into_iter().for_each(|line| {
+        let mut l: (usize, u32) = (0, 0); //ix,n
+        let mut r: (usize, u32) = (0, 0);
+
+        //left
+        for (ix, c) in line.chars().enumerate() {
+            if c.is_digit(10) {
+                l = (ix, c.to_digit(10).unwrap());
+                break;
+            }
+        }
+        //right
+        for (ix, c) in line.chars().rev().enumerate() {
+            if c.is_digit(10) {
+                r = (ix, c.to_digit(10).unwrap());
+                break;
+            }
+        }
+        res.push(format!("{}{}", l.1, r.1).parse().unwrap());
+    });
+    println!("{:?}", res.iter().sum::<i32>());
 }
 
-fn test_fn(input:&str) -> String{
-    "todo!()".to_string()
+fn test_fn(input: &str) -> String {
+    format!("{input}").to_string()
 }
 
 #[cfg(test)]
@@ -13,7 +38,19 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let res = test_fn("");
-        assert_eq!(res, "4".to_string());
+        let results = vec![12, 38, 15, 77];
+        let mut ix = 0;
+        test_fn(
+            "1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet",
+        )
+        .lines()
+        .into_iter()
+        .for_each(|dig| {
+            assert_eq!(dig, results[ix].to_string());
+            ix += 1;
+        });
     }
 }
--- aoc_2023/day-01/src/bin/part2.rs
+++ aoc_2023/day-01/src/bin/part2.rs
@@ -1,5 +1,68 @@
+use std::collections::HashMap;
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("./input1.txt");
+    let mut res = Vec::<i32>::new();
+
+    let hmap: HashMap<&str, u32> = HashMap::from_iter(vec![
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+    input.lines().into_iter().for_each(|line| {
+        let mut l: (usize, u32) = (0, 0); //ix,n
+        let mut r: (usize, u32) = (0, 0);
+
+        for (ix, c) in line.chars().enumerate() {
+            if c.is_digit(10) {
+                if l.0 == 0 && l.1 == 0 {
+                    l = (ix, c.to_digit(10).unwrap());
+                } else {
+                    r = (ix, c.to_digit(10).unwrap());
+                }
+            }
+        }
+        //duplicate right if there is only one digit
+        if r.1 == 0 && l.1 > 0 {
+            r.0 = l.0;
+            r.1 = l.1;
+        }
+
+        //build Sorted Tuple by Index
+        let mut sorted_tuple = Vec::<(usize, u32)>::new(); //(ix,num)
+        hmap.iter().for_each(|(text, digit)| {
+            //check front
+            if let Some(found_ix) = line.find(text) {
+                sorted_tuple.push((found_ix, *digit));
+            }
+            //check back
+            if let Some(found_ix) = line.rfind(text) {
+                sorted_tuple.push((found_ix, *digit));
+            }
+        });
+
+        if l.1 != 0 {
+            sorted_tuple.push((l.0, l.1));
+        }
+        if r.1 != 0 {
+            sorted_tuple.push((r.0, r.1));
+        }
+        sorted_tuple.sort_by(|a, b| a.0.cmp(&b.0)); //ASC by idx
+
+        let it = format!(
+            "{}{}",
+            sorted_tuple.first().unwrap().1,
+            sorted_tuple.last().unwrap().1
+        )
+        .parse::<i32>()
+        .unwrap();
+
+        res.push(it);
+    });
+    println!("{:?}", res.iter().sum::<i32>());
 }
-
-
